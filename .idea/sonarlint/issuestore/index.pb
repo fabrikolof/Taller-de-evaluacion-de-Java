0de21d8eff9de2a962149f250796e5b607d16120
Fabricio <fabribcom@gmail.com>
2022-06-29 08:43:17 -0300

    Ejercicios 1 con Java ternary

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+d
+4src/main/java/com/example/tallerjava1/Operacion.java,d\8\d85084eaf7ba631b42ad93c41d76569e0d4f9fde
--- src/main/java/com/example/tallerjava1/Ejercicio1/Ejercicio1.java
+++ src/main/java/com/example/tallerjava1/Ejercicio1/Ejercicio1.java
@@ -12,23 +12,24 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         int numeroUno, numeroDos;
         String mensaje;
-        numeroUno = 20;
-        numeroDos = 30;
-
+        numeroUno = 40;
+        numeroDos = 40;
 
         // Programación clásica.
-        if(numeroUno > numeroDos){ // El valor 1 es mayor al 2
+/*        if(numeroUno > numeroDos){ // El valor 1 es mayor al 2
             System.out.println("El númeo uno es mayor al número dos. Número 1: "+numeroUno+" y Número 2: "+numeroDos);
         } else if(numeroUno < numeroDos) { // Los valores son iguales
             System.out.println("El númeo uno es menor al número dos. Número 1: "+numeroUno+" y Número 2: "+numeroDos);
         } else if (numeroUno == numeroDos) { // El valor 1 es menor al dos
             System.out.println("El númeo uno y el número dos son iguales. Número 1: "+numeroUno+" y Número 2: "+numeroDos);
-        }
+        }*/
 
         // Java Ternary Operator
-/*       mensaje = (numeroUno < numeroDos) ? ("El númeo uno es mayor al número dos. Número 1: "+numeroUno+" y Número 2: "+numeroDos) : ("hola");
+       mensaje = (numeroUno > numeroDos) ? ("El número uno es mayor al número dos. Número 1: "+numeroUno+" y Número 2: "+numeroDos) :
+                    (numeroUno < numeroDos) ? ("El númeo uno es menor al número dos. Número 1: "+numeroUno+" y Número 2: "+numeroDos) :
+                       ("El númeo uno y el número dos son iguales. Número 1: "+numeroUno+" y Número 2: "+numeroDos);
 
-        System.out.println(mensaje);*/
+        System.out.println(mensaje);
 
     }
 }
